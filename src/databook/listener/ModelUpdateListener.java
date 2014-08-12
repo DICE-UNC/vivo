08bcbc1f5030d3197f9d7017a36bbc14e2b7cc8b
Xu Hao <xuh@email.unc.edu>
2014-08-12 09:26:48 -0400

    .

--- src/databook/listener/ModelUpdateListener.java
+++ src/databook/listener/ModelUpdateListener.java
@@ -92,7 +92,6 @@ public class ModelUpdateListener implements ServletContextListener {
 
 				AMQPClient.receiveMessage(AMQP_HOST, AMQP_QUEUE,
 						modelUpdater);
-
 			}
 		});
 		instance = this;
