f98a778035ac1e5f64ec4fe8b26fd80797b79d4e
xu-hao <xuh@email.unc.edu>
2014-08-12 14:40:35 -0400

    Update install_vivo.sh

--- install_vivo.sh
+++ install_vivo.sh
@@ -2,17 +2,12 @@ pwd=`pwd`
 downloads_dir=$pwd/$1
 databook_dir=$pwd/$2
 indexing_jar_path=$3
-tomcat_dir=$databook_dir/apache-tomcat-7.0.55
+tomcat_dir=/var/lib/tomcat7
 vivo_dir=$databook_dir/vivo-rel-1.5
 lib_dir=$vivo_dir/lib
-ant_dir=$databook_dir/apache-ant-1.8.4
 data_dir=$databook_dir/data
-ant_arc=apache-ant-1.8.4-bin.tar.gz
 vivo_arc=vivo-rel-1.5.tar.gz
-tomcat_arc=apache-tomcat-7.0.55.tar.gz
-ant_url=http://archive.apache.org/dist/ant/binaries/$ant_arc
 vivo_url="http://downloads.sourceforge.net/project/vivo/VIVO%20Application%20Source/vivo-rel-1.5.tar.gz"
-tomcat_url="http://apache.mirrors.hoobly.com/tomcat/tomcat-7/v7.0.55/bin/apache-tomcat-7.0.55.tar.gz"
 dbname=vivo_databook
 mysql_username=vivo_databook
 mysql_password=`< /dev/urandom tr -dc _A-Z-a-z-0-9 | head -c32`
@@ -20,19 +15,6 @@ echo generated password $mysql_password
 databook_root=root@databook
 echo please enter your mysql root password:
 read mysql_root_password
-
-
-if [ -e $downloads_dir/$ant_arc ]; then
-	echo
-else
-	pushd $downloads_dir
-	wget $ant_url
-	popd
-fi
-
-pushd $databook_dir
-tar zxvf $downloads_dir/$ant_arc 
-popd
 
 if [ -e $downloads_dir/$vivo_arc ]; then
 	echo
@@ -47,27 +29,9 @@ pushd $databook_dir
 tar zxvf $downloads_dir/$vivo_arc 
 popd
 
-if [ -e $downloads_dir/$tomcat_arc ]; then
-	echo
-else
-	pushd $downloads_dir
-	echo wget from $tomcat_url
-	wget $tomcat_url
-	popd
-fi
-
-pushd $databook_dir
-tar zxvf $downloads_dir/$tomcat_arc 
-popd
-
-# setup ant
-
-ANT_HOME=$ant_dir
-PATH=$PATH:$ant_dir/bin/ant
-
 # install & setup mysql
 
-sudo apt-get install -y mysql-server mysql-client openjdk-7-jdk gnome-icon-theme-full
+sudo apt-get install -y mysql-server mysql-client openjdk-7-jdk gnome-icon-theme-full ant tomcat7
 
 mysql -u root -p"$mysql_root_password" -e "DROP USER '$mysql_username'@'localhost';"
 mysql -u root -p"$mysql_root_password" -e "DROP DATABASE $dbname;"
